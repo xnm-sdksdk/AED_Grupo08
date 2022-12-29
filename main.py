8764feb97d7e0953b158597c0a80a0c3565b9a7e
xnm-sdksdk <n.mendonca@protonmail.com>
2022-12-29 10:51:10 +0000

    Login Configurations

--- main.py
+++ main.py
@@ -83,11 +83,36 @@ def register():
 # Login Window
 def login():
     login_window = Toplevel(main)
-    login_window.geometry("{:.0f}x{:.0f}+{:.0f}+{:.0f}".format(main_width, main_height, int(x), int(y)))
-    login_window.title("Register")
+    
+    # Different dimensions for the login window
+    login_width = 500
+    login_height = 250
+    login_window.geometry("{:.0f}x{:.0f}+{:.0f}+{:.0f}".format(login_width, login_height, int(x), int(y)))
+    login_window.title("Login")
     # register_window.resizable(0,0)
     login_window.focus_force() # Forces the focus to the current window
     login_window.grab_set() # Directs all events to the active window
+
+
+    # Login Components
+    # Name Label 
+    name_loginlbl = Label(login_window, text="Name", font=("Sans Serif", 20), fg="#000")
+    name_loginlbl.place(x=80,y=50)
+
+    # Name Entry
+    name_login_entry = Entry(login_window) # variable =
+    name_login_entry.place(x=250,y=60)
+
+    # Password Label
+    password_loginlbl = Label(login_window, text="Password", font=("Sans Serif", 20), fg="#000")
+    password_loginlbl.place(x=80,y=100)    
+    
+    # Password Entry
+    password_login_entry = Entry(login_window) # variable =
+    password_login_entry.place(x=250,y=110)
+
+
+
 
 
 
@@ -114,6 +139,12 @@ main.configure() # Background Configurations - COLOR TO BE DECIDE LATER ########
 # main.resizable(0,0) disable resizing of the app
 
 
+
+
+
+
+
+
 # Main Window Components
 
 # Name Label
@@ -132,7 +163,7 @@ passwordlbl.place(x=500,y=200)
 
 
 # Password Entry
-password_Entry = Entry(main)
+password_Entry = Entry(main) # variable =
 password_Entry.place(x=650,y=200)
 
 
@@ -142,7 +173,7 @@ login_btn.place(x=700,y=250)
 
 
 # Register Button
-register_btn = Button(main, text = 'Register', font = ('Sans Serif', 16, "bold"),width = 6, command = register) # command =
+register_btn = Button(main, text = 'Register', font = ('Sans Serif', 16, "bold"),width = 6, command = register)
 register_btn.place(x=500,y=250)
 
 # Main Window Loop
