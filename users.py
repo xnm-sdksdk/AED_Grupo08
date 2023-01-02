374f6396391f4ec0dcf71fd9fa7cec749f14e993
xnm-sdksdk <n.mendonca@protonmail.com>
2023-01-02 17:42:32 +0000

    users verifications

--- users.py
+++ users.py
@@ -1,7 +1,7 @@
 # Library's: Tkinter UI and messagebox
 from tkinter import *
 from tkinter import messagebox
-
+import os
 
 # path to users.txt file
 users_file = "files/users.txt"
@@ -32,17 +32,29 @@ def verification(name_regist_temp, age_regist_temp, email_regist_temp, password_
     # print(all_profiles) Verify if is all good
 
     
-    if name == "" or age == "" or email == "" or password == "" or user == "":
+    # verification for empty fields
+    if name_regist_temp == "" or age_regist_temp == "" or email_regist_temp == "" or password_regist_temp == "" or password_confirmation == "" or user_type == "":        
         messagebox.showerror("Error!", "All fields are required!")
-        
+        return
+    
+    
+    # verification for the password confirmation
+    if password_regist_temp != password_confirmation:
+        messagebox.showerror("Regist", "Password does not match!")
+        return 
         
     for profile_check in all_profiles:
         if name == profile_check:
-            messagebox.showerror("Error!", "Account already exists!")
+            messagebox.showerror("Regist", "Account already exists!")
             return 
             
-            
-        # else for placement of the treeview
+    
+    usersf = open(users_file, "a")
+    line = name_regist_temp + "," + age_regist_temp + "," + email_regist_temp + "," + password_regist_temp + "," + user_type + "\n"
+    usersf.write(line)
+    usersf.close()
+    messagebox.showinfo("Regist", "Account has been created!")
+    
 
 
 
