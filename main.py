88b323b78d9d95d41d459989a99e801392a39669
xnm-sdksdk <n.mendonca@protonmail.com>
2023-01-08 13:28:58 +0000

    Profile Page

--- main.py
+++ main.py
@@ -77,39 +77,40 @@ def favorites():
 
 
 def profile():
-    pass
-
-
-
-def settings():
-    
-    settingsPanel = PanedWindow(main, orient=HORIZONTAL, width= 800, height=500)
-    settingsPanel.place(x=200,y=50)
+    profilePanel = PanedWindow(main, orient=HORIZONTAL, width= 800, height=500)
+    profilePanel.place(x=200,y=50)
     
     
     # Username Info
-    usernameInfo = Label(settingsPanel, text="Username: ", font=("Sans Serif", 15, "bold"), fg="#000000")
+    usernameInfo = Label(profilePanel, text="Username: ", font=("Sans Serif", 15, "bold"), fg="#000000")
     usernameInfo.place(x=50,y=0)
     
 
     # Age Info
-    ageInfo = Label(settingsPanel, text="Age: ", font=("Sans Serif", 15, "bold"), fg="#000000")
+    ageInfo = Label(profilePanel, text="Age: ", font=("Sans Serif", 15, "bold"), fg="#000000")
     ageInfo.place(x=50,y=50)
     
     
     # Email Info
-    emailInfo = Label(settingsPanel, text="Email: ", font=("Sans Serif", 15, "bold"), fg="#000000")
+    emailInfo = Label(profilePanel, text="Email: ", font=("Sans Serif", 15, "bold"), fg="#000000")
     emailInfo.place(x=50,y=100)
     
     
     # Password Info
-    passwordInfo = Label(settingsPanel, text="Password: ", font=("Sans Serif", 15, "bold"), fg="#000000")
+    passwordInfo = Label(profilePanel, text="Password: ", font=("Sans Serif", 15, "bold"), fg="#000000")
     passwordInfo.place(x=50,y=150)
     
     
     # Display Password CheckButton
-    pwdDisplay = Checkbutton(settingsPanel, text="Display Password", font=("Sans Serif", 15, "bold"), fg="#000000")
+    pwdDisplay = Checkbutton(profilePanel, text="Display Password", font=("Sans Serif", 15, "bold"), fg="#000000")
     pwdDisplay.place(x=50,y=200)
+
+
+
+
+
+def settings():
+    pass
     
 
 
