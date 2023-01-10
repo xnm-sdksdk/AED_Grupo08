130e3b97cf84b1f74163ebc107bc97cc20137b12
xnm-sdksdk <n.mendonca@protonmail.com>
2023-01-10 23:26:03 +0000

    accessing data

--- profileSettings.py
+++ profileSettings.py
@@ -2,3 +2,23 @@
 from tkinter import *
 from tkinter import messagebox
 import os
+
+usersFile = "/home/xnm/Documents/Algoritmia_Estrutura_de_Dados/Projeto_2022_2023/AED_Project_22_23/Files/users.txt"
+
+
+
+def personal_details():
+    
+    file = open(usersFile, "r")
+    file_data = file.read()
+    details = file_data.split()
+    name = details[0]
+    age = details[1]
+    mail = details[2]
+    pwd = details[3]
+    pwdCheck = details[4]
+    user = details[5]
+    
+    
+    
+    
