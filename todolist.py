9b874ff84e4f0c1debf7bcd06e438dbcc545b718
xnm-sdksdk <n.mendonca@protonmail.com>
2023-01-10 22:56:47 +0000

    path

--- todolist.py
+++ todolist.py
@@ -2,6 +2,6 @@ from tkinter import *
 from tkinter import messagebox
 import os
 
-tasks_file = "Files\\list.txt"
+tasks_file = "/home/xnm/Documents/Algoritmia_Estrutura_de_Dados/Projeto_2022_2023/AED_Project_22_23/Files/list.txt"
 
 
