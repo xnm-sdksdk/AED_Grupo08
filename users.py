533546a4e04a7c623caf4c63466588ff4cda6b15
xnm-sdksdk <n.mendonca@protonmail.com>
2023-01-10 20:24:34 +0000

    Register and Login

--- users.py
+++ users.py
@@ -7,44 +7,21 @@ import os
 users_file = "/home/xnm/Documents/Algoritmia_Estrutura_de_Dados/Projeto_2022_2023/AED_Project_22_23/Files/users.txt"
 
 # Function to verify all inputs inserted by the user in the register function 
-"""
-def verification(name_regist_temp,password_regist_temp):
-     
+def verification(name_regist_temp, password_regist_temp):
     
-    # To check all files inside the directory
     all_profiles = os.listdir()
-    # print(all_profiles) Verify if is all good
-
     
-    usersFilePath = open(users_file, "r", encoding="utf-8") # open in read mode
-    listUsers = usersFilePath.readlines() # read file to a list
-    usersFilePath.close() # close file
     
-    for line in listUsers:
-        if name_regist_temp == line.split(";")[0]:
-            messagebox.showerror("Register", "Account already exists!")
+    fUsers = open(users_file, "r", encoding="utf-8")
+    usersList = fUsers.readlines()
+    name_check = usersList[0]
+    pwd_check = usersList[3]
+    fUsers.close()
+    
+    for name in all_profiles:
+        if name_regist_temp != name_check: 
+            messagebox.showerror("Login", "Invalid Username!")
             return
-    
-    # verification for the password confirmation
-    if password_regist_temp != password_confirmation:
-        messagebox.showerror("Regist", "Password does not match!")
-        return 
-
-        
-    for profile_check in all_profiles:
-        
-        if name_regist_temp == profile_check:
-            messagebox.showerror("Register", "Account already exists!")
-            return 
-            
-    
-    line = name_regist_temp + "," + age_regist_temp + "," + email_regist_temp + "," + password_regist_temp + "," + user_type + "\n"
-    messagebox.showinfo("Register", "Account has been created!")
-"""
-    
-    
-    
-    
 
 # Function to verify if the user is registered in the app
 def authentication(name_regist_temp, age_regist_temp, email_regist_temp, password_regist_temp, password_confirmation, user_type, panelRegister):
