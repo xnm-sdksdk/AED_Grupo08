58f578a989ff7dff3d861882ab641521bf417fbc
xnm-sdksdk <n.mendonca@protonmail.com>
2023-01-12 15:35:07 +0000

    Beginning of the notifications window

--- notifications.py
+++ notifications.py
@@ -1,4 +1,13 @@
 # Library's: Tkinter UI and messagebox
 from tkinter import *
 from tkinter import messagebox
+from plyer import notification # Library to send notifications
+import time 
 import os
+
+
+
+
+def send_Info():
+    
+    pass
