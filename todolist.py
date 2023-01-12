06d43ae5cdd63a64ab30628393d7ad49649c48a3
xnm-sdksdk <n.mendonca@protonmail.com>
2023-01-12 00:30:51 +0000

    Defining the function in the catalog

--- todolist.py
+++ todolist.py
@@ -5,3 +5,41 @@ import os
 tasks_file = "/home/xnm/Documents/Algoritmia_Estrutura_de_Dados/Projeto_2022_2023/AED_Project_22_23/Files/list.txt"
 
 
+# Function to add tasks to the list
+def addTask():
+    pass
+
+
+# Function to remove the selected task
+def deleteTask():
+    pass
+
+
+# Function to clean the whole ListBox
+def cleanBox():
+    pass
+
+
+# Function to increment the number of tasks
+def countTasks():
+    pass
+
+
+# Function to sort the tasks in ascending order
+def sortAsc():
+    pass
+
+
+# Function to sort the tasks in descending order
+def sortDesc():
+    pass
+
+
+# Function to add to the favorites list
+def addFavorites():
+    pass
+
+
+# Function to filter the tasks
+def filter():
+    pass
