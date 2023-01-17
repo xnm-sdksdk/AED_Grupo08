44749f690fe2491fc333768a0df9c0e54c4cf1a4
xnm-sdksdk <n.mendonca@protonmail.com>
2023-01-17 21:59:54 +0000

    comments file

--- favorites.py
+++ favorites.py
@@ -5,5 +5,5 @@ import os
 
 favorites_file = "/home/xnm/Documents/Algoritmia_Estrutura_de_Dados/Projeto_2022_2023/AED_Project_22_23/Files/favorites.txt"
 
-
+comments_file = "/home/xnm/Documents/Algoritmia_Estrutura_de_Dados/Projeto_2022_2023/AED_Project_22_23/Files/comments.txt"
 
