db3efe4d25248a446dce35a2a4acce2bab4c03ae
xnm-sdksdk <n.mendonca@protonmail.com>
2023-01-17 22:02:40 +0000

    Insert comment function

--- favorites.py
+++ favorites.py
@@ -1,9 +1,18 @@
 # Library's: Tkinter UI and messagebox
 from tkinter import *
 from tkinter import messagebox
+from time import strftime
+from datetime import datetime
 import os
 
 favorites_file = "/home/xnm/Documents/Algoritmia_Estrutura_de_Dados/Projeto_2022_2023/AED_Project_22_23/Files/favorites.txt"
 
 comments_file = "/home/xnm/Documents/Algoritmia_Estrutura_de_Dados/Projeto_2022_2023/AED_Project_22_23/Files/comments.txt"
 
+
+
+def insertComment(userName):
+    
+    # Register the comment by time and date
+    time = datetime.datetime.now()
+    date = time.strftime("%d/%m/%Y %H:%M:%S")
